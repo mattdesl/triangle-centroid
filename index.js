39b0e60ff17c342d0c7f0c9e9b76ce2793cc9b4b
mattdesl <dave.des@gmail.com>
2015-06-01 10:17:49 -0400

    formatting

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ function triangleCentroid (triangle) {
 
   var dimension = triangle[0].length
   var result = new Array(dimension)
-  for (var i=0; i<dimension; i++) {
+  for (var i = 0; i < dimension; i++) {
     var t0 = triangle[0][i]
     var t1 = triangle[1][i]
     var t2 = triangle[2][i]
--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 var centroid = require('./')
 var test = require('tape')
 
-test('computes the centroid of a triangle', function(t) {
-  var t2d = [ 
+test('computes the centroid of a triangle', function (t) {
+  var t2d = [
     [10, 0],
     [20, 2],
     [30, 1]
@@ -16,7 +16,7 @@ test('computes the centroid of a triangle', function(t) {
 
   t.deepEqual(centroid(t2d), [20, 1], 'handles 2d')
   t.deepEqual(centroid(t3d), [20, 1, 8], 'handles 3d')
-  t.throws(function() {
+  t.throws(function () {
     centroid([])
   }, 'throws type error')
 
